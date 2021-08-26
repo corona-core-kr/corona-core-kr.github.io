6cccceb674e290440e1de4c22b971ec9dde37a1b
tlwns5598 <tlwns5598@gmail.com>
2021-08-27 04:55:21 +0900

    1

--- 01_code/submenu_2/js/utils/Data.js
+++ 01_code/submenu_2/js/utils/Data.js
@@ -1,4 +1,4 @@
-const url = 'http://corona-core-kr.github.io//01_code/submenu_2/Data/Date/'
+// const url = 'http://corona-core-kr.github.io/01_code/submenu_2/Data/Date/'
+const url = 'http://coronacore.site/main/CoronaCore/01_code/submenu_2/Data/Date/';
 
 export {url};
-
