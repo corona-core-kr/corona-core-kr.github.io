1df303b5714aa06268c6ac1135050670a19edca1
tlwns5598 <tlwns5598@gmail.com>
2021-08-27 09:38:33 +0900

    Update Data.js

--- 01_code/submenu_2/js/utils/Data.js
+++ 01_code/submenu_2/js/utils/Data.js
@@ -1,4 +1,5 @@
-const url = 'http://corona-core-kr.github.io/01_code/submenu_2/Data/Date/'
+// const url = 'http://corona-core-kr.github.io/01_code/submenu_2/Data/Date/'
+const url = 'http://corona-core-kr.com/01_code/submenu_2/Data/Date/'
 // const url = 'http://coronacore.site/main/CoronaCore/01_code/submenu_2/Data/Date/';
 
 export {url};
