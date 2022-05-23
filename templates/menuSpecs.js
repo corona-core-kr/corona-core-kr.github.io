8bb1d3cf4b04d4d815d4c19a7d6a771fdf1a19f3
tlwns5598 <tlwns5598@gmail.com>
2022-05-23 11:23:24 +0900

    css fix sitemap

--- templates/menuSpecs.js
+++ templates/menuSpecs.js
@@ -1,28 +1,29 @@
 let menuSpecs = `
 <div id="white_box">
 
-<div id="black_box"><b>통계 정보</b><br></div><br>
-    <div class="psedu_mid_box">
-        <a href="01_code/submenu_2/graph1.html" class="submenuLink">국내 누적 확진자수</a>
-        <a href="01_code/submenu_2/graph2.html" class="submenuLink">일일 확진자 (지역|도표)</a>
-        <a href="01_code/submenu_2/graph3.html" class="submenuLink">일일 확진자 (지역|지도)</a>
-        <a href="01_code/submenu_2/graph4.html" class="submenuLink">지역별 기간별 비교</a>
-        <a href="01_code/submenu_2/graph5.html" class="submenuLink">특정 수치 초과일 조회</a>
-    </div>
+  <div id="black_box"><b>통계 정보</b><br></div><br>
+  <div class="psedu_mid_box">
+    <a href="01_code/submenu_2/graph1.html" class="submenuLink">국내 누적 확진자수</a>
+    <a href="01_code/submenu_2/graph2.html" class="submenuLink">일일 확진자 (지역|도표)</a>
+    <a href="01_code/submenu_2/graph3.html" class="submenuLink">일일 확진자 (지역|지도)</a>
+    <a href="01_code/submenu_2/graph4.html" class="submenuLink">지역별 기간별 비교</a>
+    <a href="01_code/submenu_2/graph5.html" class="submenuLink">특정 수치 초과일 조회</a>
+  </div>
 
-    <div id="black_box"><b>방역 정보</b><br></div><br>
-    <div class="psedu_mid_box">
-        <a href="01_code/submenu_3/01_corona19.html" class="indexLink">코로나 정보</a><br>
-        <a href="01_code/submenu_3/02_vaccine.html" class="indexLink">백신 정보</a><br>
-        <a href="01_code/submenu_3/04_prevention.html" class="indexLink">행동수칙</a><br><br>
-    </div>
+  <div id="black_box"><b>방역 정보</b><br></div><br>
+  <div class="psedu_mid_box">
+    <a href="01_code/submenu_3/01_corona19.html" class="submenuLink">코로나 정보</a>
+    <a href="01_code/submenu_3/02_vaccine.html" class="submenuLink">백신 정보</a>
+    <a href="01_code/submenu_3/04_prevention.html" class="submenuLink">행동수칙</a>
+  </div>
     
-    <div id="black_box"><b>정부 정책</b><br></div><br>
-    <div class="psedu_mid_box">
-        <a href="01_code/submenu_4/01_policy.html" class="submenuLink">사회적 거리두기 정책</a>
-        <a href="01_code/submenu_4/03_person_movement.html" class="submenuLink">확진자 동선</a>
-        <a href="01_code/submenu_4/03_mask.html" class="submenuLink">마스크 착용</a>
-    </div>
+  <div id="black_box"><b>정부 정책</b><br></div><br>
+  <div class="psedu_mid_box">
+    <a href="01_code/submenu_4/01_policy.html" class="submenuLink">사회적 거리두기 정책</a>
+    <a href="01_code/submenu_4/03_person_movement.html" class="submenuLink">확진자 동선</a>
+    <a href="01_code/submenu_4/03_mask.html" class="submenuLink">마스크 착용</a>
+  </div>
+
 </div>
 `;
 document.write(menuSpecs);
@@ -47,29 +48,29 @@ let menuSpecs2 = `
       <div>입력한 확진자 수치를 넘긴 날짜를 검색합니다</div>
     </div>
 
-              <div id="black_box"><b>방역 정보</b></div>
-              <div class="left-title-box">
-                <div class="each-LTB"><a href="01_code/submenu_3/01_corona19.html" class="indexLink">코로나 정보</a></div>
-                <div class="each-LTB"><a href="01_code/submenu_3/02_vaccine.html" class="indexLink">백신 정보</a></div>
-                <div class="each-LTB"><a href="01_code/submenu_3/04_prevention.html" class="indexLink">행동 수칙</a></div>
-              </div>
-              <div class="right-desc-box">
-                <div>코로나 바이러스 정보를 제공합니다</div>
-                <div>코로나 백신 정보를 제공합니다</div>
-                <div>기본적인 에방 행동 수칙을 확인할 수 있습니다 </div>
-              </div>
+    <div id="black_box"><b>방역 정보</b></div>
+    <div class="left-title-box">
+      <div class="each-LTB"><a href="01_code/submenu_3/01_corona19.html" class="indexLink">코로나 정보</a></div>
+      <div class="each-LTB"><a href="01_code/submenu_3/02_vaccine.html" class="indexLink">백신 정보</a></div>
+      <div class="each-LTB"><a href="01_code/submenu_3/04_prevention.html" class="indexLink">행동 수칙</a></div>
+    </div>
+    <div class="right-desc-box">
+      <div>코로나 바이러스 정보를 제공합니다</div>
+      <div>코로나 백신 정보를 제공합니다</div>
+      <div>기본적인 에방 행동 수칙을 확인할 수 있습니다 </div>
+    </div>
 
-              <div id="black_box"><b>정부 정책</b></div>
-              <div class="left-title-box">
-                <div class="each-LTB"><a href="01_code/submenu_4/01_policy.html" class="indexLink">사회적 거리두기 정책</a></div>
-                <div class="each-LTB"><a href="01_code/submenu_4/03_person_movement.html" class="indexLink">확진자 동선</a></div>
-                <div class="each-LTB"><a href="01_code/submenu_4/03_mask.html" class="indexLink">마스크 착용안</a></div>
-              </div>
-              <div class="right-desc-box">
-                <div>지역별 확진자 동선 링크를 제공합니다</div>
-                <div>전체/단계별 사회적 거리두기 정책 안내</div>
-                <div>마스크 착용 방칙과 위반 시 과태료 안내</div>
-              </div>
+    <div id="black_box"><b>정부 정책</b></div>
+    <div class="left-title-box">
+      <div class="each-LTB"><a href="01_code/submenu_4/01_policy.html" class="indexLink">사회적 거리두기 정책</a></div>
+      <div class="each-LTB"><a href="01_code/submenu_4/03_person_movement.html" class="indexLink">확진자 동선</a></div>
+      <div class="each-LTB"><a href="01_code/submenu_4/03_mask.html" class="indexLink">마스크 착용안</a></div>
+    </div>
+    <div class="right-desc-box">
+      <div>지역별 확진자 동선 링크를 제공합니다</div>
+      <div>전체/단계별 사회적 거리두기 정책 안내</div>
+      <div>마스크 착용 방칙과 위반 시 과태료 안내</div>
+    </div>
 
 </div>
 `;
